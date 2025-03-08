b3b8bceb44e8d8db6e7dd0db24a6b82978902c3a
dicodeit <ado.mar.2019@gmail.com>
2025-03-08 16:59:34 -0600

    add compilator

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { DefaultDataSource } from '@config/data-source';
 import { database } from '@config/database';
+import { USERS_ROUTES_PREFIX, usersRouter } from '@users/users.routes';
 import express from 'express';
 
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+
+app.use(USERS_ROUTES_PREFIX, usersRouter);
 
 const defaultDatabase = database('default', DefaultDataSource);
 defaultDatabase.initialize({
