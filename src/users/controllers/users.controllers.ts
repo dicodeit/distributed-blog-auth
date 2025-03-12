96415d25732dca4050a4762eddd5245ca95db83e
dicodeit <ado.mar.2019@gmail.com>
2025-03-11 18:37:33 -0600

    fix import

--- src/users/controllers/users.controllers.ts
+++ src/users/controllers/users.controllers.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { BaseController } from "@config/controller";
 import { service } from "@config/service";
 import { UsersService } from "@users/services/users.service";
+import { User } from '@users/entities/user.entity';
 
 export class UsersController extends BaseController {
   private readonly usersService: UsersService;
